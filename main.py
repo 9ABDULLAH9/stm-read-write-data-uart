855366be6e8dd2c80a88b35575aaedc56ad6f4d5
Mert <60283912+9ABDULLAH9@users.noreply.github.com>
2024-04-10 11:11:16 +0300

    chose port and mode

--- main.py
+++ main.py
@@ -3,3 +3,38 @@ from matplotlib.animation import FuncAnimation
 import serial.tools.list_ports
 import serial
 import csv
+
+print("Programimiza hoş geldiniz\nMeyt9\n")
+
+
+
+
+
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+
+portlist = []
+
+for onePort in ports:
+	portlist.append(str(onePort))
+	print(str(onePort))
+
+val = input ('Port seçin: COM')
+br = input ('\nBant genişliği seçin: ')
+ser = None
+
+while ser is None:
+    try:
+        ser = serial.Serial('COM' + val, br, timeout=1)
+    except serial.serialutil.SerialException as e:
+        print("\nBu com port şu anda kullanılamıyor, lütfen yeniden deneyin.")
+        val = input ('Port seçin: COM')
+
+x_vals = []
+data1 = []
+data2 = []
+
+stat = -1
+
+while ((stat != "1") & (stat != "2")):
+	stat = input("\n1-Data okuma\n2-Data gonderme\nProgram modunu seçiniz: ")
